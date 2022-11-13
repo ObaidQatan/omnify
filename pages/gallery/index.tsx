3af63d1e3150aef00bb99805c828ba37f5d12ea6
Obaid Qatan <ogaten27@gmail.com>
2022-11-14 02:52:55 +0530

    fetch bikes

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,6 +1,11 @@
+import { showNotification } from "@mantine/notifications";
 import { GetServerSideProps } from "next";
 import useTranslation from "next-translate/useTranslation";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import Loading, { loadingState } from "../../src/components/common/Loading";
+import { Bike } from "../../src/types/Bike";
 import { User } from "../../src/types/User";
 
 const Gallery = ({
@@ -12,9 +17,67 @@ const Gallery = ({
 }) => {
   const router = useRouter();
   const { t: tCommon } = useTranslation("common");
+  const [_, setLoading] = useRecoilState(loadingState);
+
+  const [bikes, setBikes] = useState<Bike[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL as string}/api/bikes`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          setLoading(false);
+
+          return showNotification({
+            title: "Error",
+            message: "Error updating bikes",
+            color: "red",
+          });
+        }
+
+        res.json().then((data) => {
+          if (data.error) {
+            console.log(data.error);
+            setLoading(false);
+
+            return showNotification({
+              title: "Error",
+              message: data.error,
+              color: "red",
+            });
+          } else {
+            showNotification({
+              title: "Success",
+              message: "Bikes fetched successfully",
+              color: "green",
+            });
+
+            setLoading(false);
+            console.log({ bikes: data.bikes });
+            return setBikes(data.bikes);
+          }
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        return showNotification({
+          title: "Error",
+          message: e,
+          color: "red",
+        });
+      });
+  }, []);
 
   return (
     <div className="gallery flex w-screen h-screen overflow-hidden bg-gray-50">
+      <Loading />
       <h1>{tCommon("bikes gallery")}</h1>
     </div>
   );
