4a317e9326d750e6edd9437ada20b271f4f754d1
Obaid Qatan <ogaten27@gmail.com>
2022-11-15 16:54:21 +0530

    update seeds

--- db/seed.ts
+++ db/seed.ts
@@ -2,8 +2,53 @@ import { PrismaClient } from "@prisma/client";
 import encrypt from "../src/util/encrypt";
 const prisma = new PrismaClient();
 
-async function main() {}
-// seed here
+async function main() {
+  // seed here
+  // remove all bikes
+  await prisma.bike.deleteMany();
+  // add many bikes
+  await prisma.bike.createMany({
+    data: [
+      {
+        name: "Bike 1",
+      },
+      {
+        name: "Bike 2",
+      },
+      {
+        name: "Bike 3",
+      },
+      {
+        name: "Bike 4",
+      },
+      {
+        name: "Bike 5",
+      },
+      {
+        name: "Bike 6",
+      },
+      {
+        name: "Bike 7",
+      },
+      {
+        name: "Bike 8",
+      },
+      {
+        name: "Bike 9",
+      },
+      {
+        name: "Bike 10",
+      },
+      {
+        name: "Bike 11",
+      },
+      {
+        name: "Bike 12",
+      },
+    ],
+  });
+}
+
 main()
   .catch((e) => {
     throw e;
